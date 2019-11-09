4a7e7459e2d1403f0cec536492a652f201df2a72
Navid Haghighi <nvdhaghighi@aol.com>
2019-11-09 18:29:09 -0500

    NH - added lambda version python script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,16 +1,26 @@
+import json
 import boto3
 import zipfile
 
-s3= boto3.resource('s3')
+def lambda_handler(event, context):
+    # TODO implement
 
-portfolio_bucket=s3.Bucket('portfolio.navid.info')
-build_bucket=s3.Bucket('portfoliobuild.navid.info')
 
-build_bucket.download_file('buildportfolio.zip', '/tmp/buildportfolio.zip')
+    s3= boto3.resource('s3')
 
-with zipfile.ZipFile('/tmp/buildportfolio.zip') as myzip:
-    for nm in myzip.namelist():
-        print(nm)
-        obj= myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_bucket=s3.Bucket('portfolio.navid.info')
+    build_bucket=s3.Bucket('portfoliobuild.navid.info')
+
+    build_bucket.download_file('buildportfolio.zip', '/tmp/buildportfolio.zip')
+
+    with zipfile.ZipFile('/tmp/buildportfolio.zip') as myzip:
+        for nm in myzip.namelist():
+            print(nm)
+            obj= myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
